27f5420922f8aabb6f73a07211201f5acee4b079
Seo MinSeok <minsoku03@inha.edu>
2023-11-12 16:55:50 +0900

    fix: study column

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -28,7 +28,31 @@ export class UserModel {
   id: number;
 
   // 제목
-  @Column()
+  @Column({
+    // 데이터베이스에서 인지하는 칼럼 타입
+    // 자동으로 유추됨
+    type: 'varchar',
+    // 데이터베이스 칼럼 이름
+    // 프로퍼티 이름으로 자동 유추됨
+    name: 'title',
+    // 값의 길이
+    // 입력 할 수 있는 글자의 길이가 300
+    length: 300,
+    // null이 가능한지
+    nullable: true,
+    // true면 처음 저장할 때만 값 지정 가능
+    // 이후에는 값 변경 불가능
+    update: true,
+    // find()를 실행할 때 기본으로 값을 불러올지
+    // 기본값이 true
+    select: true,
+    // 기본 값
+    // 아무것도 입력 안 했을 때 기본으로 입력되게 되는 값
+    default: 'default value',
+    // 칼럼중에서 유일무이한 값이 되어야하는지
+    // 이메일 ?
+    unique: false,
+  })
   title: string;
 
   // 데이터 생성일자
